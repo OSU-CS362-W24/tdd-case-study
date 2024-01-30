93e7edb31e50688d430ce8eb38808924e61690cf
Alexander Guyer <guyera@oregonstate.edu>
2024-01-29 16:29:35 -0800

    Red: Add test to rotate a to get n

--- rot13.test.js
+++ rot13.test.js
@@ -3,3 +3,7 @@ const rot13 = require('./rot13')
 test('rot13 produces empty string given empty string', function() {
 	expect(rot13("")).toBe("")
 })
+
+test('rot13 produce n given a', function() {
+	expect(rot13("a")).toBe("n")
+})
